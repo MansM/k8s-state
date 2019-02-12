0531018800f449e9be0e6a2cc0c86f0fc0e2c950
Mans Matulewicz <mans.matulewicz@gmail.com>
2019-02-12 15:26:27 +0100

    reset pvs and updated bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,10 @@ helm init --service-account tiller --upgrade --tiller-image=jessestuart/tiller:l
 
 kubectl apply -f namespaces/
 kubectl apply -f pvs/
+kubectl apply -f master.key
 kubectl apply -f sealed-secrets
+
+sleep 30
 
 echo "make sure to take the special key"
 kubectl apply -f flux/
